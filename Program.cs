28d9e340fd8e48d5edb9adf682ca75d4ca2d6c53
MogamatTayyibDawood <st10132915@vcconnect.edu.za>
2024-08-30 17:52:47 +0200

    <refrences>

--- Program.cs
+++ Program.cs
@@ -1,5 +1,12 @@
 using CLDV6212POE.Services;
 
+//References: 
+// https://learn.microsoft.com/en-us/azure/storage/common/storage-analytics-logging
+//https://stackoverflow.com/questions/77450221/how-to-retrieve-logging-config-in-azure-storage-blob-queue-and-table-service-wi
+// https://www.site24x7.com/learn/azure-storage-types.html
+// https://docs.dynatrace.com/docs/setup-and-configuration/setup-on-cloud-platforms/microsoft-azure-services/azure-integrations/azure-cloud-services-metrics/monitor-azure-storage-account
+// https://www.azadvertizer.net/azpolicyadvertizer/storage_deploy-storage-monitoring-log-analytics.html
+
 namespace CLDV6212POE
 {
     public class Program
